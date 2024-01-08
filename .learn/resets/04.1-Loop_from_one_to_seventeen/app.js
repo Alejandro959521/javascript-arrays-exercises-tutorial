678f82c0e3206f8f28ff4fe8352738d0f260fdc1
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2024-01-08 02:26:26 +0000

    ejercicios hasta el 5.1

--- .learn/resets/04.1-Loop_from_one_to_seventeen/app.js
+++ .learn/resets/04.1-Loop_from_one_to_seventeen/app.js
@@ -2,8 +2,3 @@
 for(let number = 0; number < 10; number++) {
     // Print the number
 }  
-
-for(let i=1;i<18;i++){
-
-    console.log(i)
-}
--- .learn/resets/04.4-Add_items_to_array_looping/app.js
+++ .learn/resets/04.4-Add_items_to_array_looping/app.js
@@ -1,11 +1,5 @@
 let arr = [4,5,734,43,45];
 
-for(let i=1;i<11;i++){
-
-    arr.push(Math.floor(Math.random()*100))
-
-}
-console.log(arr)
 //*****************
 // Your code here
 // you need to loop 10 times, for example, using a for loop
